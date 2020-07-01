b5b4b7f809f7ba5b38d18032b1ad1f5be799e034
c0ld574rf15h <c0ld574rf15h@gmail.com>
2020-07-01 19:24:18 +0900

    Add firebase configurations

--- vue.config.js
+++ vue.config.js
@@ -8,16 +8,16 @@ module.exports = {
       },
     },
   },
-  // css: {
-  //   loaderOptions: {
-  //     sass: {
-  //       prependData: `
-  //         @import "node_modules/bootstrap/scss/bootstrap";
-  //         @import "node_modules/bootstrap-vue/src/index.scss";
-  //         @import "@/assets/scss/common/variables.scss";
-  //         @import "@/assets/scss/common/mixins.scss";
-  //       `,
-  //     },
-  //   },
-  // },
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+          @import "node_modules/bootstrap/scss/bootstrap";
+          @import "node_modules/bootstrap-vue/src/index.scss";
+          @import "@/assets/scss/common/variables.scss";
+          @import "@/assets/scss/common/mixins.scss";
+        `,
+      },
+    },
+  },
 };
